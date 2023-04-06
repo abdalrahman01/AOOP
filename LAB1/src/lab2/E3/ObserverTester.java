0d4a6c179f3e2970053564b715ab5cd7190c7933
Abd Alrahman Atieh <abdalrahman.atieh@gmail.com>
2023-04-06 11:10:25 +0200

    added files from website

---
+++ LAB1/src/lab2/E3/ObserverTester.java
@@ -0,0 +1,32 @@
+package lab2.E3;
+
+import java.util.ArrayList;
+
+/**
+   A class for testing an implementation of the Observer pattern.
+*/
+public class ObserverTester
+{
+   /**
+      Creates a DataModel and attaches barchart and textfield listeners
+      @param args unused
+   */
+   public static void main(String[] args)
+   {
+      ArrayList<Double> data = new ArrayList<Double>();
+
+     
+      
+      for (int i = 0; i < 10; i++) {
+    	  data.add(new Double(i));
+	}
+
+      DataModel model = new DataModel(data);
+
+      TextFrame frame = new TextFrame(model);
+
+      BarFrame barFrame = new BarFrame(model);
+
+      model.attach(barFrame);
+   }
+}
