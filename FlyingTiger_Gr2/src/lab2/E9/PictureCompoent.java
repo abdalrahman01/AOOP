c7bd7416087a046b5023769c9ca49aa53c391103
Abd Alrahman Atieh <abdalrahman.atieh@gmail.com>
2023-04-23 17:58:55 +0200

    paint the picture

--- FlyingTiger_Gr2/src/lab2/E9/PictureCompoent.java
+++ FlyingTiger_Gr2/src/lab2/E9/PictureCompoent.java
@@ -1,8 +1,12 @@
 package lab2.E9;
 
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JComponent;
 
 public class PictureCompoent extends JComponent {
@@ -19,15 +23,21 @@ public class PictureCompoent extends JComponent {
 	}
 	
 	public void paint(Graphics g) {
-		
+		Graphics2D g2 = (Graphics2D)g;
+		g2.drawImage(displayedImage, null, 0,0);
 	}
 
 	public BufferedImage getDisplayedImage() {
 		return displayedImage;
 	}
 
-	public void setDisplayedImage(BufferedImage displayedImage) {
-		this.displayedImage = displayedImage;
+	public void setDisplayedImage(File fileName) {
+		try {
+			displayedImage = ImageIO.read(fileName);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	
