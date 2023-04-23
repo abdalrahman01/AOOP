348952663a0dd05ac6c1a9583d04bffe8c6b19a9
Abd Alrahman Atieh <abdalrahman.atieh@gmail.com>
2023-04-23 12:56:49 +0200

    added methods for basic operations

--- FlyingTiger_Gr2/src/lab2/E9/PictureCompoent.java
+++ FlyingTiger_Gr2/src/lab2/E9/PictureCompoent.java
@@ -1,13 +1,37 @@
 package lab2.E9;
 
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
 import javax.swing.JComponent;
 
 public class PictureCompoent extends JComponent {
 
+	private BufferedImage displayedImage;
+	private static final long serialVersionUID = 1L;
+	
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+	
+	public PictureCompoent() {
+		super();
+	}
+	
+	public void paint(Graphics g) {
+		
+	}
+
+	public BufferedImage getDisplayedImage() {
+		return displayedImage;
+	}
+
+	public void setDisplayedImage(BufferedImage displayedImage) {
+		this.displayedImage = displayedImage;
+	}
+
+	
+	
 	
 	
 
