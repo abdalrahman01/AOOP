376a478f6fea862a48fe2b6fc5687ae2823f542e
Abd Alrahman Atieh <abdalrahman.atieh@gmail.com>
2023-04-27 14:45:06 +0200

    no thing important

--- FlyingTiger_Gr2/src/lab2/E9/TestPictureComponent.java
+++ FlyingTiger_Gr2/src/lab2/E9/TestPictureComponent.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import java.io.File;
 import java.io.IOException;
@@ -14,22 +15,14 @@ public class TestPictureComponent {
 		JFrame frame = new JFrame("Test Picture Component");
 		frame.setLayout(new GridLayout());
 		frame.setSize(500, 400);
-		final PictureComponent pictureComponent = new PictureComponent();
+		final JComponent pictureComponent = new PictureComponent();
 		File img;
 		final File img2; 
-		img = new File("C:/Users/Abd Alrahman/git/AOOP/FlyingTiger_Gr2/src/lab2/E9/images/imgs/1.png");
-		img2 = new File("C:/Users/Abd Alrahman/git/AOOP/FlyingTiger_Gr2/src/lab2/E9/images/imgs/2.png");
+		img = new File("src/lab2/E9/imgs/1.png");
+		img2 = new File("src/lab2/E9/imgs/2.png");
 		
-		try {			
-			pictureComponent.setDisplayedImage(img);
-		
-		} catch (IOException e) {
-			System.err.println(e + "  " + img);
-		}
-		
+	
 
-		frame.add(pictureComponent);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JButton next = new JButton("Next Pic");
 		
@@ -37,7 +30,7 @@ public class TestPictureComponent {
 			
 			public void actionPerformed(ActionEvent event) {
 				try {			
-					pictureComponent.setDisplayedImage(img2);
+					((PictureComponent)pictureComponent).setDisplayedImage(img2);
 				
 				} catch (IOException e) {
 					System.err.println(e + "  " + img2);
@@ -47,6 +40,8 @@ public class TestPictureComponent {
 		});
 		
 		// show frame	
+		frame.add(pictureComponent);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(next);
 		frame.pack();
 		frame.setVisible(true);
