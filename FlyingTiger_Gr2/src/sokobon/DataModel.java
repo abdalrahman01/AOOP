8a7abd8c11f9afc020819407386ab0d14355fada
Piporelele <aboodegamer4@gmail.com>
2023-05-03 11:21:46 +0200

    made test class for goal

--- FlyingTiger_Gr2/src/sokobon/DataModel.java
+++ FlyingTiger_Gr2/src/sokobon/DataModel.java
@@ -1,15 +1,5 @@
 package sokobon;
 
-/**
-  keeps track of the current level (board)
-  state of the game
-  accepts controlling events to update the state
-  checks for end-of-game / winning conditions
-  notifies observers whenever necessary
- 
- @author aliab
- */
-
 public class DataModel {
 	Level currentLevel;
 	GameState state;
@@ -23,6 +13,7 @@ public class DataModel {
 	}
 	
 	public void notifyObsever() {
+		
 		
 	}
 }
