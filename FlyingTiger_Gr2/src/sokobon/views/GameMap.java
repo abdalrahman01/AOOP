0b59d74ea699eb7f5c98e2c16501c3ec3174e681
Abd Alrahman Atieh <abdalrahman.atieh@gmail.com>
2023-05-03 10:07:32 +0200

    added gamestate view

--- FlyingTiger_Gr2/src/sokobon/views/GameMap.java
+++ FlyingTiger_Gr2/src/sokobon/views/GameMap.java
@@ -1,5 +1,26 @@
 package sokobon.views;
 
-public class GameMap {
 
+import java.util.Arrays;
+
+public class GameMap {
+	String[][] map;
+	private int width, hieght;
+	public void addPlayer(int x, int y) {}; 
+	public void addMovingBox(int x, int y) {}; 
+	public void addWall(int x, int y) {}; 
+	public int getWidth() {
+		return width;
+	}
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	public int getHieght() {
+		return hieght;
+	}
+	public void setHieght(int hieght) {
+		this.hieght = hieght;
+	}
+	public String toString() {return Arrays.toString(map);}; 
+	
 }
