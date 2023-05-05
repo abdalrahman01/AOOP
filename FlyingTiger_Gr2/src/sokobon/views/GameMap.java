423e9b98d29bf92877a95b865433d55a198d6160
Abd Alrahman Atieh <abdalrahman.atieh@gmail.com>
2023-05-05 16:58:55 +0200

    added test file

--- FlyingTiger_Gr2/src/sokobon/views/GameMap.java
+++ FlyingTiger_Gr2/src/sokobon/views/GameMap.java
@@ -6,6 +6,21 @@ import java.util.Arrays;
 public class GameMap {
 	String[][] map;
 	private int width, hieght;
+	
+	public GameMap(String[][] map) {
+		
+		width = map[0].length;
+		hieght= map.length;
+		this.map = new String[hieght][width];
+		
+		// store the map 
+		for (int h = 0; h < map.length; h++) {
+			for (int w = 0; w < map[0].length; w++) {
+				this.map[h][w] = map[h][w];
+			}
+		}
+	}
+	
 	public void addPlayer(int x, int y) {}; 
 	public void addMovingBox(int x, int y) {}; 
 	public void addWall(int x, int y) {}; 
@@ -21,6 +36,13 @@ public class GameMap {
 	public void setHieght(int hieght) {
 		this.hieght = hieght;
 	}
-	public String toString() {return Arrays.toString(map);}; 
+	public String toString() {
+		String result = "";
+		for (int i = 0; i < map.length; i++) {
+			result += Arrays.toString(map[i]) + "\n";
+		}
+		return result;
+	}; 
+	 
 	
 }
