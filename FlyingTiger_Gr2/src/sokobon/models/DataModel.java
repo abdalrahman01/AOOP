3a2e47fa7c57ca350ecf03f79522f8fab6c328af
Abd Alrahman Atieh <abdalrahman.atieh@gmail.com>
2023-05-05 19:16:16 +0200

    copied code from lab2.E4.DataModel

--- FlyingTiger_Gr2/src/sokobon/models/DataModel.java
+++ FlyingTiger_Gr2/src/sokobon/models/DataModel.java
@@ -6,5 +6,49 @@ import javax.swing.event.ChangeListener;
 
 public class DataModel {
     ArrayList<ChangeListener> listeners;
+    private char[][] map; // data
+    private int width;
+    private int hieght;
 
+    public DataModel(char[][] map) {
+        listeners = new ArrayList<ChangeListener>();
+        width = map[0].length;
+        hieght = map.length;
+        this.map = new char[hieght][width];
+
+        // store the map
+        for (int h = 0; h < map.length; h++) {
+            for (int w = 0; w < map[0].length; w++) {
+                this.map[h][w] = map[h][w];
+            }
+        }
+    }
+
+    /**
+     * Constructs a DataModel object
+     * 
+     * @return the map in an char[][]
+     */
+    public char[][] getData() {
+        return (char[][]) (map.clone());
+    }
+
+    /**
+     * Attach a listener to the Model
+     * 
+     * @param c the listener
+     */
+    public void attach(ChangeListener c) {
+        listeners.add(c);
+    }
+
+    // public void update(int location, double value) {
+    // data.set(location, new Double(value));
+    // for (ChangeListener l : listeners) {
+    // l.stateChanged(new ChangeEvent(this));
+    // }
+    // }
+
+    // TODO add movePlayerUp, movePlayerDown, movePlayerRight, movePlayerLeft
+    // instead of update.
 }
