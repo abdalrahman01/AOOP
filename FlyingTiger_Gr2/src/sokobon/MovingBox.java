fb7860f9b87d7fb4107b30384026c34ff4cff08a
Ali Abedrabbo <ali.abedrabbo1@gmail.com>
2023-05-05 15:04:24 +0200

    conflicts

--- FlyingTiger_Gr2/src/sokobon/MovingBox.java
+++ FlyingTiger_Gr2/src/sokobon/MovingBox.java
@@ -1,16 +1,16 @@
 package sokobon;
 
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Rectangle;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
 import java.io.File;
 import javax.imageio.ImageIO;
 
+import java.io.File;
+
 public class MovingBox extends GameObject implements GameBox {
 	
+	
+	public MovingBox() {
+		setIconGameObject(new File("src/sokobon/resources/imgs/sokoban_icons/crate.png"));
+	}
 	
 	public void moveUp() {};
 	public void moveDown() {};
@@ -22,20 +22,4 @@ public class MovingBox extends GameObject implements GameBox {
 	 * @return
 	 */
 	public boolean hasReachedRedDot() {return true;};
-	
-	
-	public void paintComponent(Graphics g) {
-		
-		Graphics2D g2 = (Graphics2D)g;
-		File imageMovingBox = new File ("src/sokobon/resources/imgs/sokoban_icons/crate.png");
-		BufferedImage displayedImage = null;
-		try {
-			displayedImage = ImageIO.read(imageMovingBox);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-			g2.drawImage(displayedImage, null, 0,0);
-	}
-	
 }
