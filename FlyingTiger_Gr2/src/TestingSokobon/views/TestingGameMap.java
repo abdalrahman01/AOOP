42f93d805acd5950fe12a199cb989492c0384127
Abd Alrahman Atieh <abdalrahman.atieh@gmail.com>
2023-05-05 18:20:39 +0200

    ok

--- FlyingTiger_Gr2/src/TestingSokobon/views/TestingGameMap.java
+++ FlyingTiger_Gr2/src/TestingSokobon/views/TestingGameMap.java
@@ -1,13 +1,25 @@
 package TestingSokobon.views;
 import sokobon.views.*;
-
 public class TestingGameMap {
+//	char[][] map = {
+//	        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+//	        {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+//	        {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+//	        {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+//	        {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+//	        {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+//	        {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+//	        {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+//	        {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+//	        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'}
+//	    };
+//	
 	public static void main(String[] args) {
 	GameMap map = new GameMap();
 	System.out.println(map);
 	map.addPlayer(1,1);
 	System.out.println(map);
-	map.addMovingBox(3, 2);
+	map.addMovingBox(10, 10);
 	System.out.println(map);
 	map.addWall(4, 1);
 	System.out.println(map);
