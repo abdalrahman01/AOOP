d31ca7f63a773d517471f7c3a1e021c6c35f8a5b
Abd Alrahman Atieh <abdalrahman.atieh@gmail.com>
2023-05-10 14:27:18 +0200

    move(up,down,left,right) is Done!

--- FlyingTiger_Gr2/src/sokobon/GameObjects/MovingBox.java
+++ FlyingTiger_Gr2/src/sokobon/GameObjects/MovingBox.java
@@ -20,29 +20,101 @@ public class MovingBox extends GameObject implements GameBox {
 	}
 
 	public void moveUp() {
-		posRow--;
+		move(0);
 	};
 
 	public void moveDown() {
-		posRow++;
+		move(1);
 	};
 
 	public void moveLeft() {
-		posCol--;
+		move(3);
 	};
 
 	public void moveRight() {
-		posCol++;
+		move(2);
 	};
 
 	/**
-	 * If the box has Reached the the right place
+	 * moves the box in a chose direction
 	 * 
-	 * @return
+	 * @param direction choose between {0: Up, 1: Down, 2: Right, 3: Left}
 	 */
-	public boolean hasReachedRedDot() {
-		return true;
+	private void move(int direction) {
+		int oldPosRow = posRow;
+		int oldPosCol = posCol;
+
+		switch (direction) {
+			case 0: // moveUp
+				posRow--;
+				break;
+			case 1: // move Down
+				posRow++;
+				break;
+			case 2: // move Right
+				posCol++;
+				break;
+			case 3: // move Left
+				posCol--;
+				break;
+
+			default:
+				break;
+		}
+		if (!gameMap.isInRange(posRow, posCol)) { // if new position is outside of range
+			posRow = oldPosRow; // go back to place
+			posCol = oldPosCol;
+			return;
+		}
+		if (gameMap.isMarked(posRow, posCol)) { // if new position is marked
+
+			// if crate was marked (i.e moved from marker to marker)
+			if (gameMap.isMarked(oldPosRow, oldPosCol)) { // if old position was marked
+				gameMap.setGameObject(oldPosRow, oldPosCol, new Goal()); // old position
+			} else {
+				gameMap.setGameObject(oldPosRow, oldPosCol, new Floor()); // old position
+			}
+			convertToMarked();
+
+		} else if (gameMap.isEmpty(posRow, posCol)) { // if new position is empty
+
+			// (if moved from marked to unmarked )
+			if (gameMap.isMarked(oldPosRow, oldPosCol)) { // if old position was marked
+				gameMap.setGameObject(oldPosRow, oldPosCol, new Goal());// old position
+			} else {
+				gameMap.setGameObject(oldPosRow, oldPosCol, new Floor());// old position
+			}
+			convertToUnMarked();
+
+		} else {
+			// go back to place and do nothing
+			posRow = oldPosRow;
+			posCol = oldPosCol;
+			return;
+		}
+		gameMap.setGameObject(posRow, posCol, this);
 	}
+
+	private void convertToMarked() {
+		setIconGameObject(
+				new File("FlyingTiger_Gr2\\src\\sokobon\\resources\\imgs\\sokoban_icons\\cratemarked.png"));
+		setID('m');
+	}
+
+	private void convertToUnMarked() {
+		setIconGameObject(
+				new File("FlyingTiger_Gr2\\src\\sokobon\\resources\\imgs\\sokoban_icons\\crate.png"));
+		setID('o');
+	}
+
+	// /**
+	// * If the box has Reached the the right place
+	// *
+	// * @return
+	// */
+	// public boolean hasReachedRedDot() {
+	// return true;
+	// }
 
 	public char getID() {
 		return id;
