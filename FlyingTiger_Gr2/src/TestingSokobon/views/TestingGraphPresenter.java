b05d47ccdfd7c156c091fc2d480714135d2c261c
Abd Alrahman Atieh <abdalrahman.atieh@gmail.com>
2023-05-14 10:28:52 +0200

    created a test file

---
+++ FlyingTiger_Gr2/src/TestingSokobon/views/TestingGraphPresenter.java
@@ -0,0 +1,56 @@
+
+package TestingSokobon.views;
+
+import sokobon.models.DataModel;
+import sokobon.views.GameMap;
+import sokobon.views.GraphPresenter;
+import sokobon.views.ControllerWindow;
+
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+
+public class TestingGraphPresenter {
+    private static final char X = '#';
+    private static final char P = 'p';
+    private static final char O = 'o';
+
+    private static char[][] map = new char[][] { { X, X, X, X, X, X, X, X },
+            { X, ' ', ' ', ' ', ' ', ' ', ' ', X },
+            { X, ' ', ' ', ' ', ' ', ' ', ' ', X },
+            { X, ' ', ' ', O, 'g', ' ', ' ', X },
+            { X, ' ', ' ', ' ', ' ', ' ', ' ', X },
+            { X, ' ', ' ', ' ', ' ', ' ', ' ', X },
+            { X, X, X, X, X, X, X, X } };
+
+    public static void main(String[] args) {
+        JFrame gameWindow = new JFrame("Sokoban");
+
+        gameWindow.setLayout(new FlowLayout());
+
+        DataModel dataModel = new DataModel(map);
+
+        GameMap gMap = new GameMap(dataModel);
+        ControllerWindow controllerWindow = new ControllerWindow();
+
+        GraphPresenter graphPresenter = new GraphPresenter(dataModel);
+
+        dataModel.attach(gMap);
+        dataModel.addGameMap(gMap, map);
+        controllerWindow.attachGameMap(gMap);
+
+        gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gameWindow.add(gMap.gameMap);
+        gameWindow.add(controllerWindow);
+        gameWindow.pack();
+        gameWindow.setVisible(true);
+
+        // System.out.println(gMap);
+        gMap.addPlayer(2, 2);
+        // gMap.player.moveDown();
+        // gMap.player.moveRight();
+        // gMap.player.moveRight();
+        // gMap.player.moveDown();
+
+    }
+}
