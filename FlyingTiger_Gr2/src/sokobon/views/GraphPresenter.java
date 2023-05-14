a8919afce39ae7146d70c4cc1117f93168ee174a
Abd Alrahman Atieh <abdalrahman.atieh@gmail.com>
2023-05-14 10:26:58 +0200

    to testing

--- FlyingTiger_Gr2/src/sokobon/views/GraphPresenter.java
+++ FlyingTiger_Gr2/src/sokobon/views/GraphPresenter.java
@@ -1,5 +1,6 @@
 package sokobon.views;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
@@ -10,24 +11,36 @@ import sokobon.GameObject;
 public class GraphPresenter extends JPanel implements ChangeListener {
     private GameObject[][] gameObjects;
     private DataModel dataModel;
-    private int ICON_WIDTH, ICON_HEIGHT;
-    private int width, height;
+    private int ICON_width, ICON_height;
+    private int cols, rows;
 
     public GraphPresenter(DataModel dataModel) {
-        ICON_WIDTH = 20;
-        ICON_HEIGHT = 20;
+        ICON_width = 20;
+        ICON_height = 20;
         this.dataModel = dataModel;
-        width = this.dataModel.getWidth();
-        height = this.dataModel.getHeight();
+        cols = this.dataModel.getCols();
+        rows = this.dataModel.getRows();
+        setSize(ICON_width * cols + 3, ICON_height * rows + 3);
+        setLayout(null);
+        plotIcons();
 
     }
 
     public void stateChanged(ChangeEvent arg0) {
-
+        plotIcons();
     }
 
     private void plotIcons() {
-
+        gameObjects = this.dataModel.getData();
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                JLabel temp = gameObjects[row][col].getIcon();
+                temp.setSize(ICON_width, ICON_height);
+                temp.setLocation(row * ICON_width, col * ICON_height);
+                add(temp);
+            }
+        }
+        repaint();
     }
 
 }
