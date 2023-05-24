30f0334d7798f8041be06a8c1da3ac72ac1af55b
Abd Alrahman Atieh <abdalrahman.atieh@gmail.com>
2023-05-24 16:28:46 +0200

    deleted unused classes

--- FlyingTiger_Gr2/src/sokobon/GUI.java
+++
@@ -1,7 +0,0 @@
-package sokobon;
-
-public class GUI {
-	public void drawBox(GameBox box, int x, int y) {};
-	public void changeBoxColor(GameBox box, java.awt.Color newColor) {};
-	public void drawBorder(GameBox box, String[][] map ) {};
-}
