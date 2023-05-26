f88b086fa6254ab2712be20ef96384cbdae9871a
Abd Alrahman Atieh <abdalrahman.atieh@gmail.com>
2023-05-26 09:33:08 +0200

    unusable

--- FlyingTiger_Gr2/src/TestingSokobon/views/TestingGameControllers.java
+++
@@ -1,9 +0,0 @@
-package TestingSokobon.views;
-
-import sokobon.views.GameController;
-
-public class TestingGameControllers {
-    public static void main(String[] args) {
-        GameController gameController = new GameController();
-    }
-}
