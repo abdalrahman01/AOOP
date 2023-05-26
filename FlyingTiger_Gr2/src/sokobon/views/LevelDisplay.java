5043ee38f52287ddd412c6afb728e2c4982e842f
Abd Alrahman Atieh <abdalrahman.atieh@gmail.com>
2023-05-26 09:31:48 +0200

    added javadocs, made attach() inline function

--- FlyingTiger_Gr2/src/sokobon/views/LevelDisplay.java
+++ FlyingTiger_Gr2/src/sokobon/views/LevelDisplay.java
@@ -1,3 +1,8 @@
+/**
+ * @author: Abd Alrahman Atieh
+ * 
+ * this class is used to display the current level, and the total number of levels in a JLabel. 
+ */
 package sokobon.views;
 
 import javax.swing.JLabel;
@@ -8,21 +13,30 @@ import sokobon.models.DataModel;
 
 public class LevelDisplay extends JLabel implements ChangeListener {
 
+	/**
+	 * the data model
+	 */
 	DataModel dataModel;
 
+	/**
+	 * constructor for the level display. attaches the data model to the level
+	 * display. and shows the current level and.
+	 * 
+	 * @param dataModel the data model
+	 */
 	public LevelDisplay(DataModel dataModel) {
-		attach(dataModel);
-		setSize(WIDTH, HEIGHT);
-		setText("current level [" + (dataModel.getCurrnetLevel()+1) + " / " + dataModel.getLevelCount() +  "]");
-		
-	}
-
-	public void attach(DataModel dataModel) {
 		this.dataModel = dataModel;
+		setSize(WIDTH, HEIGHT);
+		setText("current level [" + (dataModel.getCurrnetLevel() + 1) + " / " + dataModel.getLevelCount() + "]");
+
 	}
 
+	/**
+	 * on state change, update the level display. with current level and total
+	 * number of levels.
+	 */
 	public void stateChanged(ChangeEvent e) {
-		setText("current level [" + (dataModel.getCurrnetLevel()+1) + " / " + dataModel.getLevelCount() +  "]");
+		setText("current level [" + (dataModel.getCurrnetLevel() + 1) + " / " + dataModel.getLevelCount() + "]");
 	}
 
 }
